2e9cc0ba23bfbb1b1fad28a7417db11b594e0e3f
Melquiceded <melqui4251@gmail.com>
2023-09-07 00:15:09 -0400

    update

---
+++ pre-install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Installing Dependecys..."
+sleep 2
+sudo apt install python3
+pip install pytube
+pip install regex
+pip install validators
+pip install moviepy
+echo "Dependecys installed!!!"
